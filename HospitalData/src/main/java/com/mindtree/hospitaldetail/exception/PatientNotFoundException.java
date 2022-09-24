4adac371bd5a1d09f92cc1a81999deff2a76f087
Dugyala-Madhu <madhuraodugyala1234@gmail.com>
2022-09-24 07:01:53 +0530

    addingData

---
+++ HospitalData/src/main/java/com/mindtree/hospitaldetail/exception/PatientNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mindtree.hospitaldetail.exception;
+
+public class PatientNotFoundException  extends RuntimeException{
+    public PatientNotFoundException(String string) {
+        super();
+    }
+}
+
